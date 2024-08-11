bedd3eb2532c79e79d73be13335ee9442916159d
oriastanjung <oriastan999@gmail.com>
2024-08-11 23:56:57 +0700

    belajar memahami Controller, View, dan REST API Get dengan sistem Caching

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,26 +3,19 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
-     *
-     * @return void
      */
     public function register(): void
     {
-        if ($this->app->environment('production') || $this->app->environment('dev')) {
-            URL::forceScheme('https');
-        }
+        //
     }
 
     /**
      * Bootstrap any application services.
-     *
-     * @return void
      */
     public function boot(): void
     {

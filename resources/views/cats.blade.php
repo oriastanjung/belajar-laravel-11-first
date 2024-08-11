ea2c95bad601a9a34f6ae403aad16adc8b2a7793
oriastanjung <oriastan999@gmail.com>
2024-08-11 23:29:19 +0700

    belajar memahami Controller, View, dan REST API Get dengan sistem Caching

--- resources/views/cats.blade.php
+++ resources/views/cats.blade.php
@@ -11,6 +11,7 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
         rel="stylesheet">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     @vite('resources/css/app.css')
 </head>
 

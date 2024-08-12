6030a419fb16f945d7eda8372f29c3e29073e137
oriastanjung <oriastan999@gmail.com>
2024-08-12 11:04:03 +0700

    setup components layout + navbar + carousel dari home

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -15,6 +15,11 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.js"></script>
+    <style>
+        *{
+            font-family: 'Inter',sans-serif
+        }
+    </style>
 </head>
 
 
